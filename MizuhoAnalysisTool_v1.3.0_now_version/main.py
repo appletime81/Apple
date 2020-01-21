a37e009270a2d099ac6f139f23ec368d769744c6
appletime81 <itrimsl8696@gmail.com>
2020-01-21 17:27:18 +0800

    Clear file
    Clear file

--- MizuhoAnalysisTool_v1.3.0_now_version/main.py
+++
@@ -1,9 +0,0 @@
-from wx import App
-from src.python.main.tabs import TabbedFrame
-import sklearn.utils._cython_blas
-import importlib_metadata
-
-if __name__ == '__main__':
-    app = App(False)
-    frame = TabbedFrame()
-    app.MainLoop()
