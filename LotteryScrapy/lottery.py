3b6aae5319667ce9db453e598f4a9ba08af03fa4
appletime81 <itrimsl8696@gmail.com>
2020-06-08 10:05:24 +0800

    Update lottery.py

--- LotteryScrapy/lottery.py
+++ LotteryScrapy/lottery.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 #001
+#002
+#003
 head_Html_lotto='https://www.pilio.idv.tw/ltobig/list.asp'
 res = requests.get(head_Html_lotto, timeout=30)
 print(res.text)
